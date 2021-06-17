01dfd0a234bcb84a943e8b92bf3e244a8dd00a01
nathanrz01 <84390952+nathanrz01@users.noreply.github.com>
2021-06-17 11:20:19 +0200

    Update Evo_Thermal_visualization_py3.py

--- Evo_Thermal_visualization_py3.py
+++ Evo_Thermal_visualization_py3.py
@@ -15,23 +15,20 @@ import threading
 from PIL import Image, ImageTk
 import tkinter as Tk
 import cv2
-#from pylab import *
 import matplotlib.pyplot as plt
-#from collections import deque
-#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-#from matplotlib.animation import FuncAnimation
-#from matplotlib import style
-#from PyQt5.QtWidgets import QApplication, QMainWindow
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from time import time
 
 
 
 class EvoThermal(FigureCanvas):
 
     def __init__(self):
-        #super(self).__init__()
+
         ### Search for Evo Thermal port and open it ###
         ports = list(serial.tools.list_ports.comports())
         portname = None
@@ -59,20 +56,19 @@ class EvoThermal(FigureCanvas):
         self.deactivate_command = (0x00, 0x52, 0x02, 0x00, 0xD8)
         self.send_command(self.activate_command)
 
-        ### Visualization window ###
+        #Fenêtre de visualisation
         self.activate_visualization = True
         self.window = Tk.Tk()
-        self.window.wm_geometry("1720x1720")
-        # self.create_text(100,100,"Test")
-        # self.canvas= Canvas(tk,width=100, height = 100, bd=0, highlightthickness=0)
-        
-        #canvas.pack()
-        #tk.ate()upd
-        self.canvas_width = 500
-        self.canvas_height = 500
-        self.canvas2 = Tk.Canvas(self.window, width=self.canvas_width, height=self.canvas_height)
+        self.window.wm_geometry("1000x1000")
+
+        self.canvas_width = 600
+        self.canvas_height = 600
+        self.canvas2 = Tk.Canvas(self.window, width=self.canvas_width, height=self.canvas_height, bg="black")
         self.canvas2.pack(side=Tk.TOP)
         self.photo = ImageTk.PhotoImage("P")
+        
+        self.frames=[]
+        self.start = None
         
         #print(self.photo)
         self.img = self.canvas2.create_image(300,200, image=self.photo)
@@ -80,33 +76,13 @@ class EvoThermal(FigureCanvas):
         self.text2.config(height=8, width=20, text='', font=("Helvetica", 22))
         self.text2.pack(side=Tk.BOTTOM)
         self.text2.config(text="Evo Thermal 33")
-        
-        self.text3 = Tk.Label(self.window)
-        self.text3.config(height=8, width=20, text='', font=("Helvetica", 25))
-        self.text3.pack(side=Tk.TOP)
-        self.text3.config(text="  Fenêtre en temp réel")
+     
 
-        self.canvas3_width = 200
-        self.canvas3_height = 300
-        self.canvas3 = Tk.Canvas(self.window, width=self.canvas_width, height=self.canvas_height)
-        self.canvas3.pack(side=Tk.TOP)
-        self.photo = ImageTk.PhotoImage("P")
-        self.img = self.canvas3.create_image(100,0, image=self.photo)
-        
-        self.canvas4_width = 200
-        self.canvas4_height = 300
-        self.canvas4 = Tk.Canvas(self.window, width=self.canvas_width, height=self.canvas_height)
-        self.canvas4.pack(side=Tk.TOP)
-        self.photo = ImageTk.PhotoImage("P")
-        self.img = self.canvas4.create_image(100,200, image=self.photo)
-        
-        self.txt = self.canvas5.create_text(300,200, "test")
-        #self.canvas.create_text(50,50,text='text')
-        
-        #G=graphe(g,-2,3,6)
-        
+
         #for i in progressbar.progressbar(range(60)):
             #time.sleep(1)
+        #progressbar.pack(side=Tk.BOTTOM)
+        
         
         self.MinAvg = []
         self.MaxAvg = []
@@ -126,88 +102,21 @@ class EvoThermal(FigureCanvas):
         self.colormap[:, 0, 2] = r
         
     def create_fig(self, parent):
-        fig = Figure()
-        self.axes = fig.subplots()
-        FigureCanvas.__init__(self, fig)
-        # self.setParent(parent)
-        # self.toolbar = NavigationToolbar(self, self)
-        # height = self.toolbar.height() + self.height()
-        # fig.set_figheight(height)
-        # self.plot()
+        root = Tk.Tk()
+        root.wm_title("Embedding in Tk")
 
-    def plot(self):
-        import sqlite3
-        var_personnage = []
-        var_temperature = []
-        with sqlite3.connect("base.db") as con:
-            cur = con.cursor()
-            cur.execute("select personne, count(*) from matable group by(personne)")
-            for personne, nb in cur.fetchall():
-                var_personnage.append(personne)
-                var_temperature.append(nb)
-
-        people = var_personnage
-        y = range(len(people))
-        error=0
-        self.axes.barh(y, var_temperature, xerr=error, align='center',color='red', edgecolor='green', height=0.5)
-        self.axes.set_yticks(y)
-        self.axes.set_yticklabels(people)
-        self.axes.invert_yaxis()  # labels read top-to-bottom
-        self.axes.set_xlabel('Pourcentage')
-        self.axes.set_title('temperature personne')
+        fig = Figure(figsize=(5, 4), dpi=100)
+        t = np.arange(0, 10, .01)
+        fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
         
-        # def ADC(dt):
- 
-        #     #vals= adc.read_adc(1, gain=GAIN) #Lecture ADC, test sans nbr aléatoire
-        #     #valeurs.appendleft(vals)
-            
-        #     valeurs.appendleft(randint(1, 100)) #Géneration nombres aléatoire, test sans ADC
- 
- 
-        #     moyenne = sum(valeurs) / len(valeurs)
-        #     listbox.delete(0, Tk.END)
-        #     for val in valeurs:
-        #         listbox.insert(Tk.END, val)
-        #     moyenne_texte.set("Moyenne : {:.2f}".format(moyenne))
-        #     line.set_data(range(SIZE), valeurs)
- 
- 
-    # SIZE = 10
-    # app = Tk.Tk()
-    # app.wm_title("Lecture de valeurs en continu")
-    # valeurs = deque([0] * SIZE, maxlen=SIZE)
-     
-    # moyenne_texte = Tk.StringVar()
-    # moyenne_texte.set("Moyenne :")
-    # moyenne_label = Tk.Label(app, textvariable=moyenne_texte, font=('', 16),
-    #                      fg="sea green")
-    # moyenne_label.grid(row=0, column=0, padx=5, pady=5)
- 
-    # listbox = Tk.Listbox(app)
-    # listbox.grid(row=1, column=0, sticky=Tk.N+Tk.EW, padx=5, pady=5)
- 
-    # style.use("ggplot")
-    # fig = Figure(figsize=(8, 5), dpi=96)
-    # ax = fig.add_subplot(111)
-    # ax.set_xlim(0, SIZE)
-    # ax.set_ylim(0, 400)
-    # ax.set_xlabel("Temps (s)")
-    # ax.set_ylabel("Valeur (points)")
-    # line, = ax.plot(range(len(valeurs)), valeurs, 'b-o')
-    # fig.set_tight_layout(True)
- 
-    # canvas = FigureCanvasTkAgg(fig, master=app)
-    # canvas.show()
-    # canvas.get_Tk_widget().grid(row=0, column=1, rowspan=2, sticky=Tk.NSEW)
- 
-    # app.grid_columnconfigure(0, weight=1)
-    # app.grid_columnconfigure(1, weight=1)
-    # app.grid_rowconfigure(1, weight=1)
- 
-    # anim = FuncAnimation(fig, ADC, interval=500)
-    # app.mainloop()
+        canvas2 = FigureCanvasTkAgg(fig, master=root)  # c'est ici que tk dessine
+        canvas2.draw()
+        canvas2.get_tk_widget().pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
         
-        
+        canvas3 = FigureCanvasTkAgg(fig, master=root)  # c'est ici que tk dessine
+        canvas3.draw()
+        canvas3.get_tk_widget().pack(side=Tk.RIGHT, fill=Tk.BOTH, expand=1)
+ 
 
     def update_GUI(self):
         
@@ -216,16 +125,31 @@ class EvoThermal(FigureCanvas):
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        frame = cv2.resize(frame, (self.canvas_width, self.canvas_height), interpolation=cv2.INTER_NEAREST)
+        frame = cv2.resize(frame, (int(self.canvas_width/2), int(self.canvas_height/2)), interpolation=cv2.INTER_NEAREST)
 
+        if len(self.frames)== 0:
+            self.frames.append(frame)
+            self.start = time()
+            
+        if time()-self.start > 60 and len(self.frames)==1:
+            self.frames.append(frame)
+            
+            
+            
         self.photo = ImageTk.PhotoImage(Image.fromarray(frame))
-        self.canvas2.itemconfig(self.img, image=self.photo)
+        self.photo1 = ImageTk.PhotoImage(Image.fromarray(self.frames[0]))
+        # image en bas à gauche
+        self.canvas2.create_image(150,int((self.canvas_height)/2), image=self.photo, anchor=Tk.NW)
+        # image en haut à gauche
+        self.canvas2.create_image(0,0, image=self.photo1, anchor=Tk.NW)
+        if len(self.frames)==2:
+            self.photo2 = ImageTk.PhotoImage(Image.fromarray(self.frames[1]))
+            # image en haut à droite
+            self.canvas2.create_image(int((self.canvas_width)/2),0, image=self.photo2, anchor=Tk.NW)
         self.window.update()
 
     def array_2_image(self, frame):
-        '''
-        This function is creating an Image from numpy array
-        '''
+
         thermal_img = frame
         im = Image.fromarray(np.uint8(thermal_img), mode="P")
         im = im.resize(size=(self.canvas_width, self.canvas_height), resample=Image.NEAREST)
@@ -310,40 +234,17 @@ class EvoThermal(FigureCanvas):
                 return False
 
     def run(self):
-        ### Get frame and print it ###
+        #on récupère frame ici
         frame = self.get_thermals()
         #print(frame)
         self.rounded_array = np.round(frame, 0)
         self.update_GUI()
 
     def stop(self):
-        ### Deactivate USB VCP output and close port ###
+
         self.send_command(self.deactivate_command)
         self.port.close()
         
-    # def graphe():
-    
-    #     moy = (AvgMax + AvgMin)/2
-    #     moy2 = dataglob/60
-    #     ecart_type = pstdev([data1, data2, data3, data4, data5, data6])
-    #     X = np.linspace(-np.pi, np.pi, 256,endpoint=True)
-    #     C,S = moy, ecart_type
-
-    #     plot(X,C)
-    #     plot(X,S)
-
-    def init_plot():
-        plt.ion()
-        plt.figure()
-        plt.title("Test d\'acqusition", fontsize=20)
-        plt.xlabel("Temps(s)", fontsize=20)
-        plt.ylabel("Tension (V)", fontsize=20)
-        plt.grid(True)
-
-    def continuous_plot(moy, fx, moy2, fx2):
-        plt.plot(moy, fx, 'bo', markersize=1)
-        plt.plot(moy2, fx2, 'ro', markersize=1)
-        plt.draw()
         
 
 
@@ -360,3 +261,5 @@ if __name__ == "__main__":
         print("test")
         evo.window.destroy()
         evo.stop()
+        
+            
